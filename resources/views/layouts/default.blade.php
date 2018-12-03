98e1048c5671836e9bca541ebf7c1b8bf8f0fbb0
gmy5210 <294182345@qq.com>
2018-12-03 10:42:14 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sample App</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <title>@yield('title', 'Sample App') = Laravel 入门</title>
 </head>
 <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+
+        @yield('content')
+        @include('layouts._footer')
+    </div>
+
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 
-    <h1>主页</h1>
+    <div class="jumbotron">
+
+        <h1>Helllo Laravel</h1>
+        <p class="lead">
+
+            你现在所爱看到的是 <a href="https://laravel-china.org/courses/laravel-essential-training-5.5">Laravel 入门</a>
+
+        </p>
+        <p>
+            一切，将从这里开始
+        </p>
+
+        <p>
+            <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
+        </p>
+
+    </div>
 
 @stop
